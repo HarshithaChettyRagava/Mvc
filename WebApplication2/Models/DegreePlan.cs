e414162b79279790d0315357b612fdf34838951a
HarshithaChettyRagava <harshitha.chettyragava@gmail.com>
2019-03-01 15:21:43 -0600

    Deleted models, added Degree.cs and Credit.cs

--- WebApplication2/Models/DegreePlan.cs
+++ WebApplication2/Models/DegreePlan.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebApplication2.Models
-{
-    public class DegreePlan
-    {
-        public int DegreePlanID { get; set; }
-        public string DegreePlanAbbrev { get; set; }
-        public string DegreePlanName { get; set; }
-
-        public ICollection<Degree> StudentID { get; set; }
-        public ICollection<Degree> DegreeID { get; set; }
-    }
-}
+﻿
--- WebApplication2/Models/Slot.cs
+++ WebApplication2/Models/Slot.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebApplication2.Models
-{
-    public class Slot
-    {
-        public int SlotID { get; set; }
-        public string Term { get; set; }
-        public string Status { get; set; }
-
-        public ICollection<DegreePlan> DegreenPlanID { get; set; }
-        public ICollection<Credit> CreditID { get; set; }
-    }
-}
+﻿
