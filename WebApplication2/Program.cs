675984b1e2a6fbf3a66a70258a534534430687d1
Zaiba <s.zaiba.ali@gmail.com>
2019-03-02 22:17:24 -0600

    Updated Program.cs

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -14,11 +14,29 @@ namespace WebApplication2
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationOnDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequireService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured while seeding the database.");
+                }
+
+            }
+            host.Run();
+               
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+                   
     }
 }
