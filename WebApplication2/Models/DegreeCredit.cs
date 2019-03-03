79e0e7053abff1c3e270cad7a59824c05959d463
Zaiba <s.zaiba.ali@gmail.com>
2019-03-02 21:52:11 -0600

    updated DegreeCredit and DegreePlan

--- WebApplication2/Models/DegreeCredit.cs
+++ WebApplication2/Models/DegreeCredit.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class DegreeCredit
+    {
+        public int DegreeCreditID { get; set; }
+
+        public ICollection<Degree> DegreeID { get; set; }
+        public ICollection<Credit> CreditID { get; set; }
+
+    }
+}
