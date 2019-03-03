c39527533a5e9fe08a947168b140499acd0dc17e
vamshi nuka <Vamshinuka@live.in>
2019-03-02 21:43:12 -0600

    Updated content in Student.cs, Slot.cs, StudentTerm.cs

--- WebApplication2/Models/StudentTerm.cs
+++ WebApplication2/Models/StudentTerm.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class StudentTerm
+    {
+        public int StudentTermID { get; set; }
+        public string StudentTermNo { get; set; }
+        public string TermAbbrev { get; set; }
+        public string TermName { get; set; }
+
+        public ICollection<Student> StudentID { get; set; }
+    }
+}
